93afa9b520e6aedcafca62c6951f67f9841e3da7
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-12-06 15:49:40 +0100

    Milestone 0: add array's imgs in DOM

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+/**Consegna:
+Prendendo come riferimento il codice scritto oggi insieme a lezione, che troverete direttamente nella mia repository di github a questo link: https://github.com/rc2pc2/js-array-objects-carousel
+Modifichiamo il codice dell'esercizio per renderlo funzionante con
+un array di oggetti, con una regola: non potete modificare l'array di oggetti in nessun caso.
+Dato un array di oggetti letterali con:
+url dell’immagine
+titolo
+descrizione
+Creare un carosello.
+
+
+Milestone 1:
+Al click dell'utente sulle frecce verso l'alto o verso il basso, l'immagine attiva diventerà visibile.
+Milestone 2:
+Aggiungiamo alla visualizzazione delle immagini anche titolo e testo relative alla singola immagine.
+Bonus 1:
+Aggiungere il ciclo infinito del carosello. Ovvero se l'immagine attiva è la prima e l'utente clicca la freccia verso l'alto,
+l'immagine che deve attivarsi sarà l'ultima e viceversa per l'ultima immagine se l'utente clicca la freccia verso il basso.
+Bonus 2:
+Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
+Bonus 3:
+Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva
+dovrà cambiare alla successiva.*/
+
+
+
+
 const images = [
    {
       image: 'img/01.webp',
@@ -25,3 +52,23 @@ const images = [
       text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
    }
 ];
+
+/*Milestone 0:
+Popoliamo dinamicamente il contenuto del carosello con i dati forniti dall'array di oggetti,
+dal js (dentro al nostro 'carousel-item', per capirci).
+*/
+
+const carouselContainer = document.querySelector('.carousel-image');
+const carouselItem = document.createElement('div');
+carouselItem.classList.add('my_carousel-item');
+carouselContainer.appendChild(carouselItem);
+
+for (let i= 0; i < images.length; i++) {
+   const currentImage = images[i];
+   carouselItem.innerHTML += 
+   `  <img src="./${currentImage.image}" alt="${currentImage.title} Image">`;
+
+};
+
+
+
